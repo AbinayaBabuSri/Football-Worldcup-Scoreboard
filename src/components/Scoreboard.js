ab8195a19f76e5fdb448b8a1f1dc4a8364d1adab
Abinaya Babu Sri <158576282+AbinayaBabuSri@users.noreply.github.com>
2024-06-11 12:25:10 +0530

    updated validations&sorting logic

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Scoreboard = () => {
-  const [matches, setMatches] = useState([
+  const [onGoingMatches, setOnGoingMatches] = useState([
     { id: 1, homeTeam: 'Uruguay', awayTeam: 'Italy', homeScore: 6, awayScore: 6 },
     { id: 2, homeTeam: 'Spain', awayTeam: 'Brazil', homeScore: 10, awayScore: 2 },
     { id: 3, homeTeam: 'Mexico', awayTeam: 'Canada', homeScore: 0, awayScore: 5 },
     { id: 4, homeTeam: 'Argentina', awayTeam: 'Australia', homeScore: 3, awayScore: 1 },
     { id: 5, homeTeam: 'Germany', awayTeam: 'France', homeScore: 2, awayScore: 2 }
   ]);
+  const [allMatches, setAllMatches] = useState([...onGoingMatches]);
   const [homeTeam, setHomeTeam] = useState('');
   const [awayTeam, setAwayTeam] = useState('');
   const [homeScore, setHomeScore] = useState(0);
@@ -15,58 +16,115 @@ const Scoreboard = () => {
   const [selectedMatchId, setSelectedMatchId] = useState('');
   const [summaryData, setSummaryData] = useState('');
   const [showSummary, setShowSummary] = useState(false); 
+  const [error, setError] = useState('');
 
   const startGame = () => {
+    if(!homeTeam || !awayTeam){
+        setError('Please enter Home Team & Away Team');
+        return;
+    }
     const newMatch = {
-      id: matches.length + 1,
+      id: allMatches.length + 1,
       homeTeam: homeTeam,
       awayTeam: awayTeam,
       homeScore: 0,
-      awayScore: 0
+      awayScore: 0,
+      startTime: new Date().getTime(),
+      finishedTime: null
     };
-    setMatches([...matches, newMatch]);
+    setAllMatches([...allMatches, newMatch]);
+    setOnGoingMatches([...onGoingMatches, newMatch]);
     setHomeTeam('');
     setAwayTeam('');
+    setError('');
   };
 
   const updateScore = () => {
     if (!selectedMatchId) {
-      console.log('Please select a match to update score');
+        setError('Please select a match to update score');
       return;
     }
 
-    const updatedMatches = matches.map(match => {
-      if (match.id === selectedMatchId) {
-        return {
-          ...match,
-          homeScore: homeScore,
-          awayScore: awayScore
-        };
-      }
-      return match;
-    });
-    setMatches(updatedMatches);
+    const updatedOnGoingMatches = onGoingMatches.map(match => {
+        if (match.id === parseInt(selectedMatchId)) {          
+          return {
+            ...match,
+            homeScore: homeScore,
+            awayScore: awayScore
+          };
+        }
+        return match;
+      });
+
+      const updatedAllMatches = allMatches.map(match => {
+        if (match.id === parseInt(selectedMatchId)) {
+          return {
+            ...match,
+            homeScore: homeScore,
+            awayScore: awayScore
+          };
+        }
+        return match;
+      });
+
+    setOnGoingMatches(updatedOnGoingMatches);
+    setAllMatches(updatedAllMatches);
     setHomeScore(0);
     setAwayScore(0);
     setSelectedMatchId('');
+    setError('');
   };
 
   const finishMatch = () => {
     if (!selectedMatchId) {
-      console.log('Please select a match to finish');
+      setError('Please select a match to finish.');
       return;
     }
 
-    const filteredMatches = matches.filter(match => match.id !== selectedMatchId);
-    setMatches(filteredMatches);
+    const updatedOngoingMatches = onGoingMatches.filter(match => match.id !== parseInt(selectedMatchId));
+    const updatedAllMatches = allMatches.map(match => {
+      if (match.id === parseInt(selectedMatchId)) {
+        return {
+          ...match,
+          finishedTime: new Date().getTime()
+        };
+      }
+      return match;
+    });
+
+    setOnGoingMatches(updatedOngoingMatches);
+    setAllMatches(updatedAllMatches);
     setSelectedMatchId('');
+    setError('');
   };
 
   const getSummary = () => {
-    const summary = matches.map(match => `${match.homeTeam} ${match.homeScore} - ${match.awayTeam} ${match.awayScore}`).join("\n");
+    const sortedMatches = [...allMatches].sort((a, b) => {
+        const totalScoreA = a.homeScore + a.awayScore;
+        const totalScoreB = b.homeScore + b.awayScore;
+        if (totalScoreA !== totalScoreB) {
+          return totalScoreB - totalScoreA; 
+        } else {
+          return a.startTime - b.startTime; 
+        } 
+      });
+  
+    const summary = sortedMatches.map(match => `${match.homeTeam} ${match.homeScore} - ${match.awayTeam} ${match.awayScore}`).join("\n");
+           
     setSummaryData(summary);
     setShowSummary(true); 
   };
+
+  useEffect(() => {
+    const selectedMatch = onGoingMatches.find(match => match.id === parseInt(selectedMatchId));
+    if(selectedMatch){
+        setHomeScore(selectedMatch.homeScore);
+        setAwayScore(selectedMatch.awayScore);
+    }else{
+        setHomeScore(0);
+        setAwayScore(0);
+    }
+  }, [selectedMatchId,onGoingMatches]);
 
   return (
     <div>
@@ -75,16 +133,17 @@ const Scoreboard = () => {
         <input type="text" value={homeTeam} placeholder="Home Team" onChange={e => setHomeTeam(e.target.value)} />
         <input type="text" value={awayTeam} placeholder="Away Team" onChange={e => setAwayTeam(e.target.value)} />
         <button onClick={startGame}>Start Game</button>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </div>
       <div>
         <select value={selectedMatchId} onChange={e => setSelectedMatchId(e.target.value)}>
           <option value="">Select Match</option>
-          {matches.map(match => (
+          {onGoingMatches.map(match => (
             <option key={match.id} value={match.id}>{`${match.homeTeam} vs ${match.awayTeam}`}</option>
           ))}
         </select>
-        <input type="number" value={homeScore} placeholder="Home Score" onChange={e => setHomeScore(Number(e.target.value))} />
-        <input type="number" value={awayScore} placeholder="Away Score" onChange={e => setAwayScore(Number(e.target.value))} />
+        <input type="number" min={0} value={homeScore} placeholder="Home Score" onChange={e => setHomeScore(Number(e.target.value))} />
+        <input type="number" min={0} value={awayScore} placeholder="Away Score" onChange={e => setAwayScore(Number(e.target.value))} />
         <button onClick={updateScore}>Update Score</button>
         <button onClick={finishMatch}>Finish Match</button>
         <button onClick={getSummary}>Get Summary</button>
@@ -92,10 +151,9 @@ const Scoreboard = () => {
       {showSummary && ( 
         <div>
           <h2>Summary</h2>
-          <h3>{summaryData}</h3>
+          <pre>{summaryData}</pre>
         </div>
       )}
-      
     </div>
   );
 };
